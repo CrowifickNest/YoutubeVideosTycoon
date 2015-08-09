8b7299139c7e22357f3742586ba691b01818dba3
BuildTools <unconfigured@null.spigotmc.org>
2015-08-09 07:15:40 -0700

    Added unity files Added Temp Texture for grass. Made a simple non functioning Player movement Script
    BUGS:
    I rotate a lot when moving
    Speed is x50000 when in air?

---
+++ Unity Files/Youtuber Tycoon Story/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed = 0.0f;
+
+    private Rigidbody rigid;
+
+    private Cursor cursor;
+
+	void Start () {
+
+        rigid = GetComponent<Rigidbody>();
+
+    }
+	
+	void Update () {
+	
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            rigid.transform.Translate(new Vector3(playerSpeed, rigid.velocity.y, rigid.velocity.z));
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            rigid.transform.Translate(new Vector3(rigid.velocity.x, playerSpeed, rigid.velocity.z));
+
+        }
+
+
+    }
+
+}
